fe4fc5801e4412a3e6dba9f494437807fcc3bba1
MelfordD <melford.deekay@gmail.com>
2021-12-13 00:35:32 -0800

    hhhhhh

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-    
-    
-    
-# Create your models here.
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
+
+
+
 
 
 class UserManager(BaseUserManager):
